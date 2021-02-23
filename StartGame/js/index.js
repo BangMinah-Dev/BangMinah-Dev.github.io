43a568991918b8b6131879a6571a611ae744cf7a
BangMinah-Dev <huytq.act@gmail.com>
2021-02-23 22:38:22 +0700

    upload

---
+++ StartGame/js/index.js
@@ -0,0 +1,47 @@
+// Js
+
+$(document).ready(function () {
+    $(".cover").slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: "linear",
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+    $(".carousel").slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
