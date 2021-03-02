ecfc64cc23fc2cdf2e9c156f6d4d09eb8d1b873a
BangMinah-Dev <huytq.act@gmail.com>
2021-03-03 05:26:26 +0700

    update

--- StartGame/js/category.js
+++ StartGame/js/category.js
@@ -1,1 +1,51 @@
 jplist.init();
+
+window.addEventListener("onload", _onload);
+
+function _onload(){
+    let card = document.getElementsByClassName("grid-item")[0];
+    let heightCard = window.getComputedStyle(card);
+    let res_card = heightCard.getPropertyValue("height");
+    let res_card_0px = res_card.slice(0, -2);
+
+    let image = document.getElementsByClassName("image")[0];
+    let heightImage = window.getComputedStyle(image);
+    let res_image = heightImage.getPropertyValue("height");
+    let res_image_0px = res_image.slice(0, -2);
+
+    // let content = document.getElementsByClassName("content")[0]
+    // let heightContent = window.getComputedStyle(content);
+    // let res_content = heightContent.getPropertyValue("height")
+
+    let resize_content = document
+        .getElementById("grid")
+        .querySelectorAll(".content");
+    for (let i = 0; i < resize_content.length; i++) {
+        resize_content[i].style.height = res_card_0px - res_image_0px + "px";
+    }
+}
+
+window.addEventListener("resize", _resize);
+
+function _resize() {
+    let card = document.getElementsByClassName("grid-item")[0];
+    let heightCard = window.getComputedStyle(card);
+    let res_card = heightCard.getPropertyValue("height");
+    let res_card_0px = res_card.slice(0, -2);
+
+    let image = document.getElementsByClassName("image")[0];
+    let heightImage = window.getComputedStyle(image);
+    let res_image = heightImage.getPropertyValue("height");
+    let res_image_0px = res_image.slice(0, -2);
+
+    // let content = document.getElementsByClassName("content")[0]
+    // let heightContent = window.getComputedStyle(content);
+    // let res_content = heightContent.getPropertyValue("height")
+
+    let resize_content = document
+        .getElementById("grid")
+        .querySelectorAll(".content");
+    for (let i = 0; i < resize_content.length; i++) {
+        resize_content[i].style.height = res_card_0px - res_image_0px + "px";
+    }
+}
