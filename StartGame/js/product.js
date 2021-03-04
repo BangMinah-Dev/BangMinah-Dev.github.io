c110c5b5e7d59fc3568417178a846a2df4433122
BangMinah-Dev <huytq.act@gmail.com>
2021-03-04 19:20:23 +0700

    update

--- StartGame/js/product.js
+++ StartGame/js/product.js
@@ -8,7 +8,40 @@ $(document).ready(function () {
     $(".image-link").magnificPopup({ type: "image" });
 });
 
-// Js
+// function checkScroll(){
+//     var startY = $('.leftBox').height() //The point where the navbar changes in px
+
+//     if($(window).scrollTop() > startY){
+//         $('.navbar').addClass("scrolled");
+//     }else{
+//         $('.navbar').removeClass("scrolled");
+//     }
+// }
+
+// if($('.leftBox').length > 0){
+//     $(window).on("scroll load resize", function(){
+//         checkScroll();
+//     });
+// }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    // let start = $("title").height()
+
+    let start = $("#title").offset().top
+
+    let _body = $(window).scrollTop()
+    if(_body > 450){
+        $("#dev").css("font-size", "0px")
+        $("#title").css({"position": "fixed", "top": "60px", "z-index" : "40"})
+    }else{
+        document.getElementById("dev").style.fontSize = "16px"
+        $("#title").css({"position": "absolute", "bottom" : "-100px", "left" : "50%", "transform" : "translate(-50%)", "height": "150px", "top" : "auto"})
+
+    }
+    console.log(start)
+}
 
 $(document).ready(function () {
     $(".carousel").slick({
