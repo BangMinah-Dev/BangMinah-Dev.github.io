c051b500339410fac0d64be86ded6d7342393ca1
BangMinah-Dev <huytq.act@gmail.com>
2021-03-05 04:55:11 +0700

    update

--- StartGame/js/product.js
+++ StartGame/js/product.js
@@ -24,24 +24,30 @@ $(document).ready(function () {
 //     });
 // }
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction(){
-    // let start = $("title").height()
+// function scrollFunction(){
+//     // let start = $("title").height()
 
-    let start = $("#title").offset().top
+//     let start = $("#title").offset().top
+    
+//     let x = window.scrollY + document.querySelector('#title').getBoundingClientRect().top // Y
+//     let xx = window.scrollY + document.querySelector('body').getBoundingClientRect().top // Y
 
-    let _body = $(window).scrollTop()
-    if(_body > 450){
-        $("#dev").css("font-size", "0px")
-        $("#title").css({"position": "fixed", "top": "60px", "z-index" : "40"})
-    }else{
-        document.getElementById("dev").style.fontSize = "16px"
-        $("#title").css({"position": "absolute", "bottom" : "-100px", "left" : "50%", "transform" : "translate(-50%)", "height": "150px", "top" : "auto"})
 
-    }
-    console.log(start)
-}
+//     let _body = $(window).scrollTop()
+//     if(_body > 450){
+//         $("#dev").css("font-size", "0px")
+//         // $("#title").css({"position": "fixed", "top": "60px", "z-index" : "40"})
+//     }else{
+//         document.getElementById("dev").style.fontSize = "16px"
+//         // $("#title").css({"position": "absolute", "bottom" : "-100px", "left" : "50%", "transform" : "translate(-50%)", "height": "150px", "top" : "auto"})
+
+//     }
+//     console.log(x)
+//     console.log("body" + xx)
+
+// }
 
 $(document).ready(function () {
     $(".carousel").slick({
