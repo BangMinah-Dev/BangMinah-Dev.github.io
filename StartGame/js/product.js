09ac5c59d994142cec94a776de390fb570d694f9
BangMinah-Dev <huytq.act@gmail.com>
2021-03-11 02:12:06 +0700

    upload

--- StartGame/js/product.js
+++ StartGame/js/product.js
@@ -8,58 +8,12 @@ $(document).ready(function () {
     $(".image-link").magnificPopup({ type: "image" });
 });
 
-// function checkScroll(){
-//     var startY = $('.leftBox').height() //The point where the navbar changes in px
-
-//     if($(window).scrollTop() > startY){
-//         $('.navbar').addClass("scrolled");
-//     }else{
-//         $('.navbar').removeClass("scrolled");
-//     }
-// }
-
-// if($('.leftBox').length > 0){
-//     $(window).on("scroll load resize", function(){
-//         checkScroll();
-//     });
-// }
-
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction(){
-//     // let start = $("title").height()
-
-//     let start = $("#title").offset().top
-    
-//     let x = window.scrollY + document.querySelector('#title').getBoundingClientRect().top // Y
-//     // let xx = window.scrollY + document.querySelector('html').getBoundingClientRect().top // Y
-
-
-//     let _body = $(window).scrollTop()
-//     if(_body > 450){
-//         $("#dev").css("font-size", "0px")
-//         // $("#title").css({"position": "fixed", "top": "60px", "z-index" : "40"})
-//     }else{
-//         document.getElementById("dev").style.fontSize = "16px"
-//         // $("#title").css({"position": "absolute", "bottom" : "-100px", "left" : "50%", "transform" : "translate(-50%)", "height": "150px", "top" : "auto"})
-
-//     }
-//     console.log(x)
-//     // console.log("body " + xx)
-
-// }
-
 
 window.onscroll = function(){
     scrollFunction()
 }
 
 function scrollFunction(){
-    
-    // let y = window.scrollY
-    // let y_html = document.querySelector('html').getBoundingClientRect().top
-
-    // let y_html = window.scrollY + document.querySelector('#tieuDe').getBoundingClientRect().top
 
     let _html = document.querySelector("html").scrollTop
 
@@ -69,12 +23,9 @@ function scrollFunction(){
     $(".effect").css("width", (navW - 30)+"px")
 
     if(_html > (el)){
-
         $('#rightBox').addClass("effect");
-        // $('#leftBox').addClass("leftBox")
     }else{
         $('#rightBox').removeClass("effect");
-        // $('#leftBox').removeClass("leftBox")
         $("#rightBox").css("width", "70%")
     }
 }
